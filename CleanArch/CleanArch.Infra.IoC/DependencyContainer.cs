aeb7b99ccc8f38130815309fc5b7ba89167b6720
Yousef Zamani <zamaniyusef@hotmail.com>
2021-03-05 18:09:42 +0330

    Add Logger Manager Project

--- CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -1,17 +1,13 @@
-﻿using CleanArch.Application.Interfaces;
-using CleanArch.Application.Services;
-using CleanArch.Domain.Interfaces;
-using CleanArch.Infra.Data.Repository;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace CleanArch.Infra.IoC
+﻿namespace CleanArch.Infra.IoC
 {
+    using CleanArc.LoggerService;
+    using CleanArch.Application;
+    using Microsoft.Extensions.DependencyInjection;
     public class DependencyContainer
     {
         public static void RegisterServices(IServiceCollection services)
         {
-            services.AddScoped<ICourseService, CourseService>();
-            services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<ILoggerManager, LoggerManager>();
         }
     }
 }
